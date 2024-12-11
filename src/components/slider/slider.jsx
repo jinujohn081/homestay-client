4dc25912603e5be0ed645f3d6845f4466f61f475
jinu <jinujohn081@gmail.com>
2024-12-11 23:48:42 +0100

    slider updates

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,6 +1,28 @@
 import './slider.scss';
-function Slider() {
-  return <div className='slider'>slider</div>;
+function Slider({images}) {
+  return <div className='slider'>
+    <div className="fullSlider">
+      <div className="arrow">
+        <img src='/arrow.png'/>
+      </div>
+      <div className="imageContainer">
+        <img src={images[0]} alt=''/>
+      </div>
+      <div className="arrow">
+      <img src='/arrow.png' className='right'/>
+      </div>
+      
+    </div>
+<div className="close">x</div>
+    <div className="bigImage">
+      <img src={images[0]} alt=''/>
+    </div>
+    <div className="smallImages">
+      {images.slice(1).map((image,index)=>(
+<img src={image} key={index}/>
+      ))}
+    </div>
+  </div>;
 }
 
 export default Slider;
